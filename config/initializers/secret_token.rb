375a506a9172829d8e58370f6eeed98f3375df9e
hash4di <hash4di@doubledrones.com>
2011-06-16 17:13:53 +0200

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hemicars::Application.config.secret_token = 'd8c866ada96a31f4a70469f51b3a5a09812f9184b459accfe12fce8d2d22ce7783dcfde528c525537f5091093270a630e34434eb0b6a54038babc7f78ad25b9c'
